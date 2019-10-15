0d555a3452c9240eab6eded6fc8d8a9cd65680a8
Codee Ong <codeeong@gmail.com>
2019-10-15 16:20:12 +0800

    add ref to jquery

--- interface.php
+++ interface.php
@@ -1,6 +1,8 @@
 <html>
 
 <head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
 	<title>Interface</title>
 	<style>
 		.overlay {
@@ -45,7 +47,7 @@
 	  
 	</style>
 	
-	<script>
+	<script type='text/javascript'>
 		function askQuestion() {
 			console.log("asking question");
 			var askPanel = document.getElementById("askQn");	
